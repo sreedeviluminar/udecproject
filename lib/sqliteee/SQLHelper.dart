40223f3c77ad3378b016c8c1d4194d341bc4bebd
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-03-28 15:27:24 +0530

    commit

--- lib/sqliteee/SQLHelper.dart
+++ lib/sqliteee/SQLHelper.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
-
   ///create table with nametodo and column name as title and task
   static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE todo(
@@ -14,13 +13,51 @@ class SQLHelper {
   }
 
   //create database
-  static Future<sql.Database> db() async{
-     return sql.openDatabase(
-       'mytodo.db',
-       version: 1,
-       onCreate: (sql.Database database, int version) async{
-         await createTables(database);
-       }
-     );
+  static Future<sql.Database> db() async {
+    return sql.openDatabase('mytodo.db', version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTables(database);
+    });
+  }
+
+  static Future<int> addTask(String title, String task) async {
+    final db = await SQLHelper.db();
+    final data = {'title': title, 'task': task};
+    final id = await db.insert('todo', data);
+    return id;
+  }
+
+//read all the data from db u can use rawQuery() function too
+  static Future<List<Map<String, dynamic>>> readData() async {
+    final db = await SQLHelper.db();
+    return db.query('todo', orderBy: "id");
+  }
+
+  //to read single data by id
+  static Future<List<Map<String, dynamic>>> readSingleData(int id) async {
+    final db = await SQLHelper.db();
+    return db.query('todo', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  // updating table data
+  static Future<int> updateTask(int id, String title, String task) async {
+    final db = await SQLHelper.db();
+    final data = {
+      'title': title,
+      'task': task,
+      'createdAt': DateTime.now().toString()
+    };
+    final result = await db.update('todo', data);
+    return result;
+  }
+
+  //delete Task
+  static Future<void> deleteTask(int id) async {
+    final db = await SQLHelper.db();
+    try {
+      await db.delete('todo', where: "id =?", whereArgs: [id]);
+    } catch (error) {
+      debugPrint("Some error occured $error");
+    }
   }
 }
--- lib/sqliteee/mainnn.dart
+++ lib/sqliteee/mainnn.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:udecproject/sqliteee/SQLHelper.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
+}
 
 class MainPage extends StatefulWidget {
   @override
@@ -6,10 +13,37 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  var isLoading = true; // to check whether the screen have data or not
+
+  List<Map<String, dynamic>> data_from_db = []; // list to store data from db
+
+  void getData() async {
+    // to read datas from db
+    final data = await SQLHelper.readData();
+    setState(() {
+      data_from_db = data; // set data from db to our list
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // load data from db when app starts
+    super.initState();
+    getData();
+  }
+
   final title = TextEditingController();
   final task = TextEditingController();
 
   void showForm(int? id) async {
+    if (id != null) {
+      final existing_data = data_from_db.firstWhere((
+          element) => element['id'] == id);
+      title.text = existing_data['title'];
+      task.text = existing_data['task'];
+    } // fetching data from the particular id for updation
+
     showModalBottomSheet(
         context: context,
         elevation: 5,
@@ -20,7 +54,10 @@ class _MainPageState extends State<MainPage> {
                 top: 15,
                 left: 15,
                 right: 15,
-                bottom: MediaQuery.of(context).viewInsets.bottom + 120),
+                bottom: MediaQuery
+                    .of(context)
+                    .viewInsets
+                    .bottom + 120),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.end,
@@ -39,10 +76,10 @@ class _MainPageState extends State<MainPage> {
                         await createItem();
                       }
                       if (id != null) {
-                        // await updateItem(id);
+                        await updateItem(id);
                       }
                       title.text = "";
-                      task.text  ="";
+                      task.text = "";
                       Navigator.pop(context);
                     },
                     child: Text(id == null ? "Create Item" : "Update Item"))
@@ -55,15 +92,56 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("TODo")),
+      appBar: AppBar(title: const Text("TODo")),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showForm(null),
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
+      body: isLoading
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
+          itemCount: data_from_db.length,
+          itemBuilder: (ctx, index) {
+            return Card(
+              color: Colors.orange,
+              child: ListTile(
+                title: Text(data_from_db[index]['title']),
+                subtitle: Text(data_from_db[index]['task']),
+                trailing: Wrap(
+                  children: [
+                    IconButton(
+                        onPressed: () =>
+                            showForm(data_from_db[index]["id"]), // update task
+                        icon: const Icon(Icons.edit)),
+                    IconButton(
+                        onPressed: () =>
+                            deleteTask(data_from_db[index]["id"]),
+                        // delete task
+                        icon: const Icon(Icons.delete))
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 
   Future<void> createItem() async {
+    await SQLHelper.addTask(title.text, task.text);
+    getData(); // for refreshing the ui when new task is added
+  }
 
+  void deleteTask(int id) async {
+    await SQLHelper.deleteTask(id);
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("SuccessFully Deleted")));
+    getData();
+  }
+
+  Future<void> updateItem(int id) async {
+    await SQLHelper.updateTask(id, title.text, task.text);
+    getData(); //refresh ui
   }
 }
